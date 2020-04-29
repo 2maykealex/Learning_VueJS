132435a4888c8ccec812d82aba14f5626ba38f10
2maykealex <mayke.suporte@gmail.com>
2020-04-28 20:59:45 -0400

    my component

--- vue-components/src/main.js
+++ vue-components/src/main.js
@@ -1,5 +1,14 @@
 import Vue from 'vue'
 
+Vue.component('my-component', {
+  data: function () {
+    return {
+      titulo: 'Curso de VueJS',
+    }
+  },
+  template: "<h1>{{titulo}}</h1>",
+})
+
 new Vue({
   el: "#app",
 })
